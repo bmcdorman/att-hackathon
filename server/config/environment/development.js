6d465a1d03a69b93fcf0232c8989d9d57bdf09b1
Ali Hajimirza <a92hm@yahoo.com>
2014-10-20 01:05:12 -0500

    init

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://54.165.54.220/att'
+  },
+
+  seedDB: true
+};
