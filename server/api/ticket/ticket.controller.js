d176d2a251ca1af820fcd8e7bff1e9072ac06f3a
Ali Hajimirza <a92hm@yahoo.com>
2014-10-20 20:06:58 -0500

    obkect id

--- server/api/ticket/ticket.controller.js
+++ server/api/ticket/ticket.controller.js
@@ -22,7 +22,7 @@ exports.show = function(req, res) {
 
 // Get a single ticket
 exports.byUser = function(req, res) {
-  Ticket.find({'user_id':ObjectId(req.params.id)}, function (err, ticket) {
+  Ticket.find({'user_id': ObjectId(req.params.id)}, function (err, ticket) {
     if(err) { return handleError(res, err); }
     if(!ticket) { return res.send(404); }
     return res.json(ticket);
