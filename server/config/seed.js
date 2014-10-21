0478f56bb54a94cc6e0745f5eba546b762950dee
Nikhil Mutalik <nmutalik@gmail.com>
2014-10-21 08:00:42 -0500

    form bullshit

--- server/config/seed.js
+++ server/config/seed.js
@@ -32,41 +32,43 @@ var Quote = require('../api/quote/quote.model');
 //   console.log(f);
 // });
 
-// Ticket.find({}).remove(function(){});
-// Lead.find({}).remove(function(){});
-// Quote.find({}).remove(function(){});
+Ticket.find({}).remove(function() {});
+Lead.find({}).remove(function() {});
+Quote.find({}).remove(function() {});
 
-User.findOne({email: 'nikhil@ou.edu'}, function (err, user) {
-  Lead.create({
-    name: "Peter Parker",
-    street: "175 Fifth Ave",
-    city: "New York",
-    zipCode: "10010",
-    phone: "374 867 5309",
-    verified: false,
-    available_services: "AVPN",
-    competitor_services: "Verizon",
-    verified_gps: false,
-    verified_call: false,
-    verified_VASA: false,
-  }, function (err, lead) {
-    Ticket.create({
-      user_id: user._id,
-      lead_id: lead._id,
-      status: 1,
-      open: true
-    }, function (err, ticket) {
-      Quote.create ({
-        ticket_id: ticket._id,
-        std_price: 1000,
-        given_price: 800,
-        service: "AVPN and MIS"
-      }, function (err, quote) {
-        console.log(user);
-        console.log(lead);
-        console.log(ticket);
-        console.log(quote);
-      });
-    });
-  });
+User.findOne({
+	email: 'nikhil@ou.edu'
+}, function(err, user) {
+	Lead.create({
+		name: "Peter Parker",
+		street: "175 Fifth Ave",
+		city: "New York",
+		zipCode: "10010",
+		phone: "374 867 5309",
+		available_services: "AVPN",
+		competitor_services: "Verizon",
+		verified_gps: false,
+		verified_call: false,
+		verified_VASA: false
+	}, function(err, lead) {
+		Ticket.create({
+			user_id: user._id,
+			lead_id: lead._id,
+			status: 1,
+			open: true
+		}, function(err, ticket) {
+			Quote.create({
+				ticket_id: ticket._id,
+				std_price: 1000,
+				given_price: 800,
+				verified: false,
+				service: "AVPN and MIS"
+			}, function(err, quote) {
+				console.log(user);
+				console.log(lead);
+				console.log(ticket);
+				console.log(quote);
+			});
+		});
+	});
 });
