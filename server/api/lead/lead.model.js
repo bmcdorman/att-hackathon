a0de02ecafeea59010fd7c68ff4757dfee40bcfb
Ali Hajimirza <a92hm@yahoo.com>
2014-10-21 01:05:05 -0500

    new db

--- server/api/lead/lead.model.js
+++ server/api/lead/lead.model.js
@@ -7,8 +7,14 @@ var mongoose = require('mongoose'),
 var LeadSchema = new Schema({
   name: {type: String, trim: true},
   phone: {type: String, trim: true},
-  address: {type: String, trim: true},
-  verified: {type: Boolean, trim: true, default:false},
+  street: {type: String, trim: true},
+  city: {type: String, trim: true},
+  zipCode: {type: String, trim: true},
+  availible_services: {type: String, trim: true, default:''},
+  competitor_services: {type: String, trim: true, default:''},
+  verified_gps: {type: Boolean, trim: true, default:false},
+  verified_call: {type: Boolean, trim: true, default:false},
+  verified_VASA: {type: Boolean, trim: true, default:false}
 });
 
 module.exports = mongoose.model('Lead', LeadSchema);
